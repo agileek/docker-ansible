eb882f60e0a2ba14d18ef95a3161e7439a860b3e
Michael Bitard <bitard.michael@gmail.com>
2014-10-25 22:19:08 +0200

    use phusion image + wait for docker image to start before trying to ansibilize

---
+++ STOP_AND_DELETE_ALL.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+docker stop developer_base_image_instance && docker rm developer_base_image_instance
---
+++ enter.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker exec -ti developer_base_image_instance /bin/bash
